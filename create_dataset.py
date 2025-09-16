5df14459c0c04cfedc0e09f1f257bcf8d8cb8ca8
Priyanshu Pulak <priyanshu.pulak11@gmail.com>
2025-09-16 15:54:45 +0530

    improves dataset consistency and prevents invalid samples in training

--- create_dataset.py
+++ create_dataset.py
@@ -4,34 +4,36 @@ import mediapipe as mp
 import cv2
 
 mp_hands = mp.solutions.hands
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-
-hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
+hands = mp_hands.Hands(static_image_mode = True, min_detection_confidence = 0.3)
 
 DATA_DIR = Path('./Data')
 
 data = []
 labels = []
+
 for dir_path in DATA_DIR.iterdir():
     for img_path in dir_path.iterdir():
-        data_aux = []
 
         img = cv2.imread(str(img_path))
         img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
         results = hands.process(img_rgb)
+
         if results.multi_hand_landmarks:
-            for hand_landmarks in results.multi_hand_landmarks:
-                for i in range(len(hand_landmarks.landmark)):
-                    x = hand_landmarks.landmark[i].x
-                    y = hand_landmarks.landmark[i].y
-                    data_aux.append(x)
-                    data_aux.append(y)
+            hand_landmarks = results.multi_hand_landmarks[0]
+            data_aux = []
 
-            data.append(data_aux)
-            labels.append(dir_path.name)
+            for lm in hand_landmarks.landmark:
+                data_aux.append(lm.x)
+                data_aux.append(lm.y)
 
-output_file = Path("data2.pickle")
+            if len(data_aux) == 42:
+                data.append(data_aux)
+                labels.append(dir_path.name)
+            else:
+                print(f"Incomplete landmarks in image : {img_path}, skipping...")
+        else:
+            print(f"No hand landmarks detected in image: {img_path}")
+
+output_file = Path("data.pickle")
 with output_file.open('wb') as f:
     pickle.dump({'data': data, 'labels': labels}, f)
