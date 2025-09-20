e37f5a7d90e3b4b5869e197c8a004a7adceb588f
Priyanshu Pulak <priyanshu.pulak11@gmail.com>
2025-09-20 17:33:50 +0530

    Augment hand landmarks with random rotations to improve model generalization

--- create_dataset.py
+++ create_dataset.py
@@ -2,6 +2,8 @@ from pathlib import Path
 import pickle
 import mediapipe as mp
 import cv2
+import math
+import random
 
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(static_image_mode = True, min_detection_confidence = 0.3)
@@ -10,6 +12,18 @@ DATA_DIR = Path('./Data')
 
 data = []
 labels = []
+
+def rotate_landmarks(data_aux, angle_deg):
+    angle_rad = math.radians(angle_deg)
+    cos_a, sin_a = math.cos(angle_rad), math.sin(angle_rad)
+    rotated = []
+
+    for i in range(0, len(data_aux), 2):
+        x, y = data_aux[i], data_aux[i+1]
+        x_new = x * cos_a - y * sin_a
+        y_new = x * sin_a + y * cos_a
+        rotated.extend([x_new, y_new])
+    return rotated
 
 for dir_path in DATA_DIR.iterdir():
     if dir_path.is_dir():
@@ -39,6 +53,12 @@ for dir_path in DATA_DIR.iterdir():
                 if len(data_aux) == 42:
                     data.append(data_aux)
                     labels.append(dir_path.name)
+                    
+                    for _ in range(2):
+                        angle = random.uniform(-15, 15)
+                        rotated = rotate_landmarks(data_aux, angle)
+                        data.append(rotated)
+                        labels.append(dir_path.name)
                 else:
                     print(f"Incomplete landmarks in image : {img_path}, skipping...")
             else:
